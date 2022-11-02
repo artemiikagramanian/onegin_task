8dd667a112f0519d2eaeee8b25945a7805a6653a
артемий каграманян <111202188+artemiikagramanian@users.noreply.github.com>
2022-11-02 21:21:06 +0300

    Delete operations.h

--- operations.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPERATIONS_H
-#define OPERATIONS_H
-
-int reverse_strcmp (char* a_ptr, int a_size, char* b_ptr, int b_size);
-
-void swap_elems (void* a_ptr, void* b_ptr, size_t size);
-
-#endif
